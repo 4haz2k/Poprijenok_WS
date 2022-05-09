f7c2d494e3e5fa9cbd0b16a63bb285b4ed3c10ae
AS2005071645\Пользователь <zytia@mail.ru>
2022-05-10 00:34:37 +0300

    done dictionary done class library done some unit tests

---
+++ Poprijenok/UnitTesting/UnitTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WSUniversalLib;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void Test_of_get_quantity_of_product()
+        {
+            Calculation calculation = new Calculation();
+
+            int expected = 114147;
+            int actual = calculation.GetQuantityForProduct(3, 1, 15, 20, 45);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
